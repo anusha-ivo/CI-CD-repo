123dc9d22a9838c270b5e65d3e837811ecd1e555
anusha-ivo <anusha525@gmail.com>
2026-03-05 17:30:09 +0530

    changes

--- src/main/java/com/example/order/management2/Controller.java
+++ src/main/java/com/example/order/management2/Controller.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @GetMapping("/welcome")
     public  String show(){
-        return "hi from cicd";
+        return "hi from cicd with completed";
     }
 }
